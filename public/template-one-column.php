c3cf0a23706676b05e3e3f10f722b648eb01064a
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-19 16:53:11 -0500

    Add one colum template for simple pages.

---
+++ public/template-one-column.php
@@ -0,0 +1,31 @@
+<?php
+    /**
+    * Template Name: One Column Page
+    */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <?php get_template_part('head'); ?>
+
+    <body <?php body_class(); ?>>
+
+        <?php get_header(); ?>
+
+        <main>
+            <div class="section white">
+                <div class="small-container">
+                    <div class="full">
+                        <h1 class="text-center">Privacy Policy</h1>
+                        <p>Proin eu metus mi. Sed venenatis ac nisl id suscipit. Nullam sed tincidunt erat. Donec sollicitudin quam dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent felis tortor, aliquet at viverra tempor, consectetur sit amet mi. Fusce lacus mauris, dapibus in ultricies eu, facilisis vitae elit. Sed finibus velit non tincidunt elementum. Nunc laoreet ligula consectetur eros dapibus, laoreet mollis leo molestie. Cras a ultrices lorem. Vestibulum et pretium nulla, id placerat nisi.</p>
+
+                        <p>Suspendisse vestibulum interdum vulputate. Vestibulum ex ipsum, posuere vel malesuada et, pharetra vel dui. Etiam tellus lacus, posuere eget consectetur et, fermentum ut velit. Vivamus eu finibus ipsum, luctus lacinia leo. Suspendisse non congue mi. Phasellus elementum diam quis auctor pretium. Pellentesque placerat in orci a volutpat. Nunc at velit massa.</p>
+
+                        <p>Nam nisi metus, commodo non tristique sit amet, ornare vel nisi. Vivamus lacinia odio ut augue consequat, ac ornare odio tristique. Morbi porttitor velit et mauris convallis placerat. Nullam vel urna blandit, imperdiet leo nec, gravida metus. Phasellus in diam nulla. Maecenas ut sapien congue, tristique erat id, malesuada tortor. Sed facilisis, neque nec placerat dapibus, arcu nunc blandit elit, sit amet euismod est metus quis augue.</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <?php get_footer(); ?>
+
+    </body>
+</html>
