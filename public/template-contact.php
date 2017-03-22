33ba99997924d21a40cfcd505b64f5c87afda82e
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-22 15:07:59 -0500

    Finish contact template.

---
+++ public/template-contact.php
@@ -0,0 +1,28 @@
+<?php
+    /**
+    * Template Name: Contact Page
+    */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <?php get_template_part('head'); ?>
+
+    <body <?php body_class(); ?>>
+
+        <?php get_header(); ?>
+
+        <main>
+            <div class="section no-padding white image-with-text">
+                <img src="<?php the_field('hero_image') ?>">
+                <h2 class="text-center"><?php the_field('headline'); ?></h2>
+            </div>
+            <div class="section large white micro-padding-top">
+                <div class="container">
+                    <p class="text-center"><?php the_field('form_snippet') ?></p>
+                </div>
+            </div>
+        </main>
+
+        <?php get_footer(); ?>
+
+    </body>
+</html>
