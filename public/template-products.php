e9bb8e9fb7234cfcd7a0896afc91573668608373
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-23 20:47:00 -0500

    finish q club page.

--- public/template-products.php
+++ public/template-products.php
@@ -13,77 +13,23 @@
         <main>
             <div class="section tiny">
                 <div class="fluid-container">
-                    <div class="third">
-                        <h2 class="">The Lineup</h2>
-                    </div>
-                    <div class="">
-                        <h2 class="text-center">The Lineup</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="section micro torn-paper">
-                <div class="full-container">
-                    <div class="full">
-                        <div class="products-previews-wrapper">
-                            <div class="product">
-                                <img class="thumbnail">
-                                <div class="full-container promo-overlay">
-                                    <div class="graphic">
-                                        <img>
-                                    </div>
-                                    <div class="info">
-                                        <h3>American Royal Beef Rub</h3>
-                                        <p>Something about the best american royal beef rub around.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="product">
-                                <img class="thumbnail">
-                                <div class="full-container promo-overlay">
-                                    <div class="graphic">
-                                        <img>
-                                    </div>
-                                    <div class="info">
-                                        <h3>Russian Royal Beef Rub</h3>
-                                        <p>Something about the best american royal beef rub around.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="product">
-                                <img class="thumbnail">
-                                <div class="full-container promo-overlay">
-                                    <div class="graphic">
-                                        <img>
-                                    </div>
-                                    <div class="info">
-                                        <h3>African Royal Beef Rub</h3>
-                                        <p>Something about the best american royal beef rub around.</p>
-                                    </div>
-                                </div>
-                            </div>
+                    <?php
+                        $products_page = get_pages(array(
+                            'meta_key' => '_wp_page_template',
+                            'meta_value' => 'template-products.php',
+                        ))[0];
+
+                        $pages = get_children(array('post_parent' => $products_page->ID));
+
+                        foreach($pages as $page) {
+                    ?>
+                        <div class="third">
+                            <a class="product-group-thumbnail" href="<?php echo get_permalink($page->ID); ?>">
+                                <img src="<?php the_field('homepage_product_group_image', $page->ID); ?>">
+                                <h3 class="text-center"><?php echo $page->post_title; ?></h3>
+                            </a>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="section white">
-                <div class="fluid-container">
-                    <div class="full">
-                        <h2 class="text-center">Recipes Worth Rubbin'</h2>
-                    </div>
-                </div>
-                <?php include(locate_template('three-page-previews.php', false, false)); ?>
-            </div>
-            <div class="section orange">
-                <div class="fluid-container">
-                    <div class="full text-center">
-                        <h2 class="capitalize">The Smokin' Hottest Club Around.</h2>
-                    </div>
-                </div>
-                <div class="container">
-                    <div class="full text-center">
-                        <p>Sign up for the Q club to get pro-style tips, tricks and the tastiest recipes on this side of the Mason Dixon. Get ready to walk, talk, grill and smoke like a pitmaster.</p>
-                        <a href="" class="button capitalize">Sign Up Now</a>
-                    </div>
+                    <?php } ?>
                 </div>
             </div>
         </main>
