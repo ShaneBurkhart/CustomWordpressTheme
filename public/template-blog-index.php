8d15d6992ab2f58b697edfcb747cdffe086e6c9c
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-24 18:51:28 -0500

    Add blog index page.

--- public/template-blog-index.php
+++ public/template-blog-index.php
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
+<?php
+    /**
+    * Template Name: Blog Page
+    */
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <?php get_template_part('head'); ?>
 
@@ -9,34 +13,34 @@
         <main>
             <div class="section no-padding white image-with-text">
                 <img src="<?php the_field('banner_image') ?>">
-                <h2 class="text-center">Blog Posts</h2>
+                <h2 class="text-center">Recipes</h2>
             </div>
                 <?php
                     $page_num = get_query_var('paged') ? get_query_var('paged') : 1;
                     $offset = 9 * ($page_num - 1);
                     $all_pages = get_posts(array(
-                        'category_name' => 'uncategorized',
+                        'category_name' => 'recipe',
                         'posts_per_page' => -1
                     ));
-                    $posts_on_page = array_slice($all_pages, $offset, 9);
+                    $recipes_on_page = array_slice($all_pages, $offset, 9);
                 ?>
             <div class="section white micro">
                 <?php
-                    $pages = array_slice($posts_on_page, 0, 3);
+                    $pages = array_slice($recipes_on_page, 0, 3);
                     include(locate_template('three-page-previews.php', false, false));
                 ?>
             </div>
 
             <div class="section white micro">
                 <?php
-                    $pages = array_slice($posts_on_page, 3, 3);
+                    $pages = array_slice($recipes_on_page, 3, 3);
                     include(locate_template('three-page-previews.php', false, false));
                 ?>
             </div>
 
             <div class="section white micro">
                 <?php
-                    $pages = array_slice($posts_on_page, 6, 3);
+                    $pages = array_slice($recipes_on_page, 6, 3);
                     include(locate_template('three-page-previews.php', false, false));
                 ?>
             </div>
@@ -47,13 +51,13 @@
                         <?php
                             if (sizeof($all_pages) > $offset + 9) {
                         ?>
-                                <a class="button" href="/blog/page/<?php echo $page_num + 1; ?>">More Posts</a>
+                                <a class="button" href="/recipes/page/<?php echo $page_num + 1; ?>">More Recipes</a>
                         <?php } ?>
 
                         <?php
                             if ($offset > 0) {
                         ?>
-                                <a class="button" href="/blog/page/<?php echo $page_num - 1; ?>">Back</a>
+                                <a class="button" href="/recipes/page/<?php echo $page_num - 1; ?>">Back</a>
                         <?php } ?>
                     </div>
                 </div>
