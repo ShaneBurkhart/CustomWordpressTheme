0f01459ab079b2b09685e927d3dec8cec61d70a3
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-25 16:53:00 -0500

    Fix some mobile optimization.

--- public/store-locator-and-newsletter-signup-section.php
+++ public/store-locator-and-newsletter-signup-section.php
@@ -1,11 +1,16 @@
-<div class="section no-padding white image-with-two-popups">
-    <img class="bg" src="<?php echo get_template_directory_uri() ?>/assets/images/banners/free-sample-subtle-background.png">
-    <div class="popup-left">
-        <h3>Find me some Twist'd Q.</h3>
-        <?php gravity_form(3, false, false, false, '', false); ?>
-    </div>
-    <div class="popup-right">
-        <h4>Get pro-style tips, tricks and recipes straight to your inbox.</h4>
-        <?php gravity_form(3, false, false, false, '', false); ?>
+<div class="section white image-with-two-popups" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/banners/free-sample-subtle-background.png);">
+    <div class="fluid-container">
+        <div class="half">
+            <div class="popup text-left">
+                <h3>Find me some Twist'd Q.</h3>
+
+            </div>
+        </div>
+        <div class="half text-right">
+            <div class="popup">
+                <h4>Get pro-style tips, tricks and recipes straight to your inbox.</h4>
+                <?php gravity_form(3, false, false, false, '', false); ?>
+            </div>
+        </div>
     </div>
 </div>
