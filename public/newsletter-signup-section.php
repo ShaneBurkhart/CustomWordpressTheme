897b2e9924d93a5e229cf4e1b929a146b1faa83b
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-24 20:39:11 -0500

    Add newsletter signup form section.

--- public/newsletter-signup-section.php
+++ public/newsletter-signup-section.php
@@ -2,5 +2,6 @@
     <img class="bg" src="<?php echo get_template_directory_uri() ?>/assets/images/banners/free-sample-subtle-background.png">
     <div class="popup">
         <h4>Get pro-style tips, tricks and recipes straight to your inbox.</h4>
+        <?php gravity_form(3, false, false, false, '', false); ?>
     </div>
 </div>
