0c6b8702b016ba27d22dda4e255a7031766cfffd
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-26 21:45:20 -0500

    Finish mobile optimizing thank you page.

--- public/newsletter-signup-section.php
+++ public/newsletter-signup-section.php
@@ -1,7 +1,10 @@
-<div class="section no-padding white image-with-popup">
-    <img class="bg" src="<?php echo get_template_directory_uri() ?>/assets/images/banners/free-sample-subtle-background.png">
-    <div class="popup">
-        <h4>Get pro-style tips, tricks and recipes straight to your inbox.</h4>
-        <?php gravity_form(3, false, false, false, '', false); ?>
+<div class="section white image-with-popup" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/banners/free-sample-subtle-background.png);">
+    <div class="fluid-container">
+        <div class="full text-center">
+            <div class="popup">
+                <h4>Get pro-style tips, tricks and recipes straight to your inbox.</h4>
+                <?php gravity_form(3, false, false, false, '', false); ?>
+            </div>
+        </div>
     </div>
 </div>
--- public/template-thank-you.php
+++ public/template-thank-you.php
@@ -11,8 +11,7 @@
         <?php get_header(); ?>
 
         <main>
-            <div class="section no-padding white image-with-glow">
-                <img src="<?php echo get_template_directory_uri() ?>/assets/images/banners/free-sample-ty-splatter.png">
+            <div class="section no-padding white image-with-glow" style="background-image: url(<?php the_field('hero_image'); ?>);">
                 <div class="content">
                     <h1><?php the_field('headline'); ?></h1>
                     <p><?php the_field('subheading'); ?></p>
@@ -20,7 +19,14 @@
             </div>
             <div class="section tiny white micro-padding-top">
                 <h3 class="text-center capitalize">Something Else You Might Enjoy</h3>
-                <?php include(locate_template('three-page-previews.php', false, false)); ?>
+                <?php
+                    $pages = get_posts(array(
+                        'category_name' => 'uncategorized',
+                        'posts_per_page' => 3
+                    ));
+
+                    include(locate_template('three-page-previews.php', false, false));
+                ?>
             </div>
             <?php include(locate_template('newsletter-signup-section.php', false, false)); ?>
         </main>
