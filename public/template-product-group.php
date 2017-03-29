c5b13f994c60213a9f5ff630d4611fe9ac84c890
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-29 00:25:28 -0500

    Add text under product thumbnails.

--- public/template-product-group.php
+++ public/template-product-group.php
@@ -40,6 +40,7 @@
                                 <div class="product">
                                     <a href="<?php echo get_permalink($page->ID); ?>">
                                         <img class="thumbnail" src="<?php the_field('product_image', $page->ID); ?>">
+                                        <h4><?php echo $page->post_title; ?></h4>
                                     </a>
                                     <div class="full-container promo-overlay">
                                         <div class="graphic background" style="background-image: url(<?php the_field('promo_image', $page->ID); ?>);">
