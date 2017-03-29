d78d75641db4a5bbd32dc7f7d8c31a7e759b5f06
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-28 20:34:39 -0500

    Add desktop header.

--- public/header.php
+++ public/header.php
@@ -1,9 +1,27 @@
+<?php
+    $products_page = get_pages(array(
+        'meta_key' => '_wp_page_template',
+        'meta_value' => 'template-products.php',
+    ))[0];
+
+    $pages = get_children(array('post_parent' => $products_page->ID));
+?>
+
 <header class="section no-padding">
     <div class="fluid-container">
         <div class="full">
             <a id="site-logo" href="/"></a>
             <ul>
-                <li><a href="/products">Products</a></li>
+                <li>
+                    <a href="/products">Products</a>
+                    <ul class="drop-down-items">
+                        <?php
+                            foreach($pages as $page) {
+                        ?>
+                                <li><a href="<?php echo get_permalink($page->ID); ?>"><?php echo $page->post_title; ?></a></li>
+                        <?php } ?>
+                    </ul>
+                </li>
                 <li><a href="/q-club">Q' Club</a></li>
                 <li><a href="/recipes">Recipes</a></li>
                 <li><a href="/blog">Blog</a></li>
