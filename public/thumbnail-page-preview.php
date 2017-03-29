c852d209ebd74f5a07a76204375a80ac7811296c
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-28 20:55:19 -0500

    Add thumbnail overlays to post previews.

--- public/thumbnail-page-preview.php
+++ public/thumbnail-page-preview.php
@@ -1,4 +1,14 @@
-<article class="thumbnail-page-preview">
+<?php
+$thumbnail_class = 'blog-post';
+
+if (in_category('recipe', $page)) {
+    $thumbnail_class = 'recipe-post';
+} else if (in_category('recipe', $page) && get_post_format($page) == 'video') {
+    $thumbnail_class = 'video-post';
+}
+?>
+
+<article class="thumbnail-page-preview <?php echo $thumbnail_class; ?>">
     <div class="image-wrapper" style="background-image: url('<?php the_field('preview_image', $page->ID); ?>">
     </div>
     <h3><?php echo $page->post_title ?></h3>
