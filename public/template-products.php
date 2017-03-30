3ec3b5b27589de094eee0e2701189124cfd518b9
Shane Burkhart <shaneburkhart@gmail.com>
2017-03-29 23:57:48 -0500

    Fix products page.

--- public/template-products.php
+++ public/template-products.php
@@ -14,7 +14,7 @@
             <div class="section tiny">
                 <div class="container">
                     <div class="full">
-                        <h1><?php the_field('headline'); ?></h1>
+                        <h1 class="text-center"><?php the_field('headline'); ?></h1>
                         <p><?php the_field('description'); ?></p>
                     </div>
                 </div>
@@ -35,24 +35,24 @@
                 foreach($pages as $page) {
             ?>
                 <div class="section tiny product-section" style="background-image: url(<?php the_field('products_page_background_image', $page->ID); ?>);">
-                    <div class="container">
-                        <?php if ($i % 2 == 0) { ?>
-                            <div class="graphic">
-                                <a class="product-group-thumbnail" href="<?php echo get_permalink($page->ID); ?>">
-                                    <img src="<?php the_field('homepage_product_group_image', $page->ID); ?>">
-                                </a>
+                    <div class="fluid-container">
+                        <?php if ($i % 2 == 1) { ?>
+                            <div class="description left">
+                                <h3><?php echo $page->post_title; ?></h3>
+                                <p><?php the_field('products_page_description', $page->ID); ?></>
+                                <p><a href="<?php echo get_permalink($page->ID); ?>" class="button circle-arrow fa fa-arrow-right"></a></p>
                             </div>
                         <?php }?>
-                        <div class="description">
-                            <h3><?php echo $page->post_title; ?></h3>
-                            <p><?php the_field('products_page_description', $page->ID); ?></>
-                            <p><a href="<?php echo get_permalink($page->ID); ?>" class="button circle-arrow fa fa-arrow-right"></a></p>
+                        <div class="graphic">
+                            <a class="product-group-thumbnail" href="<?php echo get_permalink($page->ID); ?>">
+                                <img src="<?php the_field('homepage_product_group_image', $page->ID); ?>">
+                            </a>
                         </div>
-                        <?php if ($i % 2 == 1) { ?>
-                            <div class="graphic">
-                                <a class="product-group-thumbnail" href="<?php echo get_permalink($page->ID); ?>">
-                                    <img src="<?php the_field('homepage_product_group_image', $page->ID); ?>">
-                                </a>
+                        <?php if ($i % 2 == 0) { ?>
+                            <div class="description right">
+                                <h3><?php echo $page->post_title; ?></h3>
+                                <p><?php the_field('products_page_description', $page->ID); ?></>
+                                <p><a href="<?php echo get_permalink($page->ID); ?>" class="button circle-arrow fa fa-arrow-right"></a></p>
                             </div>
                         <?php }?>
                     </div>
