16b5148808406d8a02fd265b96f428c75b021fa1
Shane Burkhart <shaneburkhart@gmail.com>
2017-04-03 14:45:43 -0500

    fix

--- public/template-product-group.php
+++ public/template-product-group.php
@@ -33,7 +33,7 @@
                                 ));
 
                                 $page_children_ids = array();
-                                $width = floor(100.0 / sizeof($pages) * 10000) / 10000.0;
+                                $width = floor(100.0 / sizeof($pages) * 100) / 100.0;
 
                                 foreach($pages as $page) {
                                     $page_children_ids[] = $page->ID;
