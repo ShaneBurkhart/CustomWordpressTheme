da423a31fe0a94fbb3111e21ee368d15324087b5
Shane Burkhart <shaneburkhart@gmail.com>
2017-04-03 16:59:31 -0500

    Fix

--- public/search.php
+++ public/search.php
@@ -43,10 +43,10 @@
                                         } else if (get_field('page_description')) {
                                             $matches = array();
                                             $page_desc = get_field('page_description');
-                                            preg_match('/(?:<p>|^)(.+?)(?:<\/p>|\n|$)/', $page_desc, $matches);
+                                            preg_match('/(?:<p>|^)(.+?)(?:<\/p>|$)/', $page_desc, $matches);
                                     ?>
 
-                                        <p><?php echo $matches[0]; ?></p>
+                                        <p><?php echo $matches[1]; ?></p>
                                     <?php } else { ?>
                                         <?php the_excerpt(); ?>
                                     <?php } ?>
