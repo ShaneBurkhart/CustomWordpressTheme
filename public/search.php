16a500bdf2bece8d58bb37048bb9d179869f2c87
Shane Burkhart <shaneburkhart@gmail.com>
2017-04-03 16:59:56 -0500

    t

--- public/search.php
+++ public/search.php
@@ -43,7 +43,8 @@
                                         } else if (get_field('page_description')) {
                                             $matches = array();
                                             $page_desc = get_field('page_description');
-                                            preg_match('/(?:<p>|^)(.+?)(?:<\/p>|$)/', $page_desc, $matches);
+                                            preg_match('/(?:<p>|^)(.+?)(?:<\/p>|\n|$)/', $page_desc, $matches);
+                                            echo var_dump($matches);
                                     ?>
 
                                         <p><?php echo $matches[1]; ?></p>
